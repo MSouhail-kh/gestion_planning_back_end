3e961c992866a78143b4cdd55a37725383360237
M.Souhail-Khalal <hgh765469@gmail.com>
2025-02-27 23:08:44 +0000

    Update config.py
    Signed-off-by: M.Souhail-Khalal <hgh765469@gmail.com>

--- config.py
+++ config.py
@@ -2,10 +2,11 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
+
 class Config:
-    SQLALCHEMY_DATABASE_URI = "postgresql://datasigmatex_user:p2FlDdrlENg46LqTh4thWxHPAry7DZvn@dpg-cv07nrqn91rc73flmsag-a/datasigmatex"
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://datasigmatex_user:p2FlDdrlENg46LqTh4thWxHPAry7DZvn@dpg-cv07nrqn91rc73flmsag-a/datasigmatex")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = os.path.join(os.getcwd(), "uploads")
     ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg", "pdf", "xls", "xlsx"}
-    SECRET_KEY = "https://jwt.io/#debugger-io?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.cThIIoDvwdueQB468K5xDc5633seEFoqwxjF_xSJyQQ"
-    FLASK_DEBUG = True
+    SECRET_KEY = os.getenv("SECRET_KEY", "https://jwt.io/#debugger-io?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.cThIIoDvwdueQB468K5xDc5633seEFoqwxjF_xSJyQQ")   
+    FLASK_DEBUG = os.getenv("FLASK_DEBUG", "True").lower() == "true"
