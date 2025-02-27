d421b07da3f73b7d22fb67c3de30604e5c7f37ff
M.Souhail-Khalal <hgh765469@gmail.com>
2025-02-27 17:16:30 +0000

    Update config.py
    Signed-off-by: M.Souhail-Khalal <hgh765469@gmail.com>

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 class Config:
-    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg://openpg:12345678@localhost:5432/DataSigmatex"
+    SQLALCHEMY_DATABASE_URI = "postgresql://datasigmatex_user:p2FlDdrlENg46LqTh4thWxHPAry7DZvn@dpg-cv07nrqn91rc73flmsag-a/datasigmatex"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = r"C:\React\reactcode\public\static\uploads"
     ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg", "pdf", "xls", "xlsx"}
