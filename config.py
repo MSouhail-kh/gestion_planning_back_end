f0bc14c3b2a05790ecabd4e3d88f0ca5bde005bc
M.Souhail-Khalal <hgh765469@gmail.com>
2025-02-27 17:04:14 +0000

    Update config.py
    Signed-off-by: M.Souhail-Khalal <hgh765469@gmail.com>

--- config.py
+++ config.py
@@ -5,11 +5,7 @@ load_dotenv()
 class Config:
     SQLALCHEMY_DATABASE_URI = "postgresql+psycopg://openpg:12345678@localhost:5432/DataSigmatex"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    UPLOAD_FOLDER = "C:\React\reactcode\public\static\uploads"
-
+    UPLOAD_FOLDER = r"C:\React\reactcode\public\static\uploads"
     ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg", "pdf", "xls", "xlsx"}
-
     SECRET_KEY = "https://jwt.io/#debugger-io?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.cThIIoDvwdueQB468K5xDc5633seEFoqwxjF_xSJyQQ"
-
     FLASK_DEBUG = True
