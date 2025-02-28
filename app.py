3a6ffe58ed7c94e05728237e9125870811d87913
M.Souhail-Khalal <hgh765469@gmail.com>
2025-02-28 11:28:33 +0000

    Update app.py
    Signed-off-by: M.Souhail-Khalal <hgh765469@gmail.com>

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 import os
 from config import Config
-from models import db, Position
+from models import db, Position  # Import de Position pour l'insertion automatique
 from flask_migrate import Migrate
 from flask_cors import CORS
 from pythocode import main as main_blueprint
@@ -10,24 +10,27 @@ from flask_mail import Mail
 app = Flask(__name__)
 app.config.from_object(Config)
 
-# Initialize extensions
 db.init_app(app)
 migrate = Migrate(app, db)
-CORS(app, supports_credentials=True, origins="https://gestion-planning-git-gestion-planning-msouhail-khs-projects.vercel.app")
+
+# Allow all origins
+CORS(app, supports_credentials=True, origins="*")
+
 mail = Mail(app)
 
-os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
+app.register_blueprint(main_blueprint)
 
-@app.before_first_request
-def initialize_database():
+# Création des tables et insertion automatique des positions
+with app.app_context():
+    db.create_all()
+
+    # Vérifier si les données existent déjà pour éviter les doublons
     if Position.query.count() == 0:
         positions = [Position(id=i, name=f"Chaîne {i}") for i in range(1, 7)]
-        db.session.bulk_save_objects(positions)
+        db.session.add_all(positions)
         db.session.commit()
         print("✅ Données Position insérées avec succès !")
 
-# Register blueprint
-app.register_blueprint(main_blueprint)
-
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8080)
+
