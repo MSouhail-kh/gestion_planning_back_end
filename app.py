f981151829d55ee3a77cb0b1820394783ca329dc
M.Souhail-Khalal <hgh765469@gmail.com>
2025-02-28 08:57:44 +0000

    Update app.py
    Signed-off-by: M.Souhail-Khalal <hgh765469@gmail.com>

--- app.py
+++ app.py
@@ -10,9 +10,7 @@ from flask_mail import Mail
 app = Flask(__name__)
 app.config.from_object(Config)
 
-# # Ensure the upload folder exists
-# if not os.path.exists(app.config["UPLOAD_FOLDER"]):
-#     os.makedirs(app.config["UPLOAD_FOLDER"])
+os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
 
 db.init_app(app)
 migrate = Migrate(app, db)
