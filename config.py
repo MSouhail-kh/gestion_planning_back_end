cf7838b39e91381ea48d446a080ea81ea01adc29
M.Souhail-Khalal <hgh765469@gmail.com>
2025-02-28 07:56:38 +0000

    Update config.py
    Signed-off-by: M.Souhail-Khalal <hgh765469@gmail.com>

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ load_dotenv()
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://datasigmatex_user:p2FlDdrlENg46LqTh4thWxHPAry7DZvn@dpg-cv07nrqn91rc73flmsag-a/datasigmatex")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    UPLOAD_FOLDER = "https://gestion-planning-git-gestion-planning-msouhail-khs-projects.vercel.app/static/uploads"
+    UPLOAD_FOLDER = "https://gestion-planning-git-gestion-planning-msouhail-khs-projects.vercel.app/public/static/uploads"
     ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg", "pdf", "xls", "xlsx"}
     SECRET_KEY = os.getenv("SECRET_KEY", "https://jwt.io/#debugger-io?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.cThIIoDvwdueQB468K5xDc5633seEFoqwxjF_xSJyQQ")   
     FLASK_DEBUG = os.getenv("FLASK_DEBUG", "True").lower() == "true"
